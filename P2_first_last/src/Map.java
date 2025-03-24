3af13b4ce0218bb492ed13eef8d51026dc7d210d
1916453 <1916453@01-18138.pusd.dom>
2025-03-24 14:00:26 -0700

    Queue is done but the commit I made last Sunday somehow didn't save?

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -30,10 +30,14 @@ public class Map {
         }
     }
 
+
     public int findCoinBFS(int startRow, int startCol) {
         Queue<Tile> queue = new LinkedList<>();
         queue.add(map[startRow][startCol]);
         visited[startRow][startCol] = true;
+
+        // Store the previous tile to reconstruct the path
+        Tile[][] previous = new Tile[map.length][map[0].length];  
 
         int steps = 0;
         while (!queue.isEmpty()) {
@@ -41,24 +45,52 @@ public class Map {
             for (int i = 0; i < size; i++) {
                 Tile current = queue.poll();
 
-                if (current.getType() == 'C') return steps; // Found the coin
+                if (current.getType() == 'C') { 
+                    printPath(previous, current, startRow, startCol);
+                    return steps; 
+                }
 
                 for (int j = 0; j < 4; j++) {
                     int newRow = current.getRow() + dRow[j];
                     int newCol = current.getCol() + dCol[j];
 
                     if (isValidMove(newRow, newCol)) {
+                        Tile nextTile = map[newRow][newCol];
                         visited[newRow][newCol] = true;
-                        queue.add(map[newRow][newCol]);
+                        queue.add(nextTile);
+                        previous[newRow][newCol] = current; // Store parent for path reconstruction
                     }
                 }
             }
-            steps++; // Move to the next step
+            steps++;
         }
         return -1; // No path found
     }
 
-    /** DFS (Stack-Based) - Alternative search method */
+
+    private void printPath(Tile[][] previous, Tile coinTile, int startRow, int startCol) {
+        List<Tile> path = new ArrayList<>();
+        Tile current = coinTile;
+
+        // Trace the code
+        while (current != null && !(current.getRow() == startRow && current.getCol() == startCol)) {
+            path.add(current);
+            current = previous[current.getRow()][current.getCol()];
+        }
+
+        path.add(map[startRow][startCol]); // Add start position
+        Collections.reverse(path); // Reverse to get path from start to coin
+
+        // Print path
+        System.out.println("Coordinates :");
+        for (Tile tile : path) {
+            System.out.println("(" + tile.getRow() + ", " + tile.getCol() + ")");
+        }
+    }
+
+
+
+
     public int findCoinDFS(int startRow, int startCol) {
         Stack<Tile> stack = new Stack<>();
         stack.push(map[startRow][startCol]);
@@ -68,7 +100,7 @@ public class Map {
         while (!stack.isEmpty()) {
             Tile current = stack.pop();
 
-            if (current.getType() == 'C') return steps; // Found the coin
+            if (current.getType() == '$') return steps; // Found the coin
 
             for (int j = 0; j < 4; j++) {
                 int newRow = current.getRow() + dRow[j];
